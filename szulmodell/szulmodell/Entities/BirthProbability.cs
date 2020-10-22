63777e2edb7808fa259644b0546685b87d1b7647
Dávid John <johnnydawe99@gmail.com>
2020-10-22 15:05:27 +0200

    vege

---
+++ szulmodell/szulmodell/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szulmodell.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set;}
+        public int ChildNum { get; set; }
+        public double P { get; set; }
+    }
+}
