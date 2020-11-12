193f7489269cd4d3da7babbcbe18d2c666b589d0
Dávid John <johnnydawe99@gmail.com>
2020-11-12 22:14:51 +0100

    kozepe

---
+++ mikulas/mikulas/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using mikulas.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mikulas.Entities
+{
+    public class BallFactory :IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
--- mikulas/mikulas/Form1.cs
+++ mikulas/mikulas/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using mikulas.Abstractions;
+using mikulas.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,45 @@ namespace mikulas
 {
     public partial class Form1 : Form
     {
+        private List<Toy> _toys = new List<Toy>();
+
+        private IToyFactory _factory;
+        public IToyFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var toy in _toys)
+            {
+                toy.MoveToy();
+                if (toy.Left > maxPosition)
+                    maxPosition = toy.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _toys[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
+            }
         }
     }
 }
